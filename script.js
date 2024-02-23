5ca85ebca7e9fdbdce62cdcec9362561643b8b95
MalbhagePornima <142093558+MalbhagePornima@users.noreply.github.com>
2024-02-23 15:55:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click",()=>{
+	let text=document.getElementById("text").value;
+	let delay=document.getElementById("delay").value;
+
+	if(text && delay){
+		let myPromise=new Promise((resolve)=>{
+			setTimeout(()=>{
+				resolve(text);
+			},delay)
+		});
+	}
+});
